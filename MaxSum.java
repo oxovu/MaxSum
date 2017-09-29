f7c192c5c86c2f9d19a8c8816eb4bd8cd39ab7d1
oxovu <vlada318@live.com>
2017-09-29 22:08:17 +0300

    на входе и выходе метода одинаковый тип, добавила тесты и внесла исправления

--- MaxSum.java
+++ MaxSum.java
@@ -1,18 +1,21 @@
-import java.util.ArrayList;
-
 public class MaxSum {
 
-    static ArrayList<Integer> findBestDays(int a[]) {
+    static int[] findMaxSum(int a[]) {
         int sum = 0;
         int max = 0;
         int end = 0;
         int start = 0;
-        ArrayList<Integer> res = new ArrayList<>();
-        if (a.length == 0) return res;
-        while (a[start] < 0 && start != a.length - 1) {
+        if (a.length == 0) return new int[0];
+        while (a[start] <= 0 && start != a.length - 1) {
             start++;
         }
-        if (start == a.length - 1) return res;
+        if (start == a.length - 1) if (a[start] <= 0) {
+            return new int[0];
+        } else {
+            int res[] = new int[1];
+            res[0] = a[start];
+            return res;
+        }
         for (int i = start; i < a.length; i++) {
             if (sum < 0) {
                 sum = 0;
@@ -24,7 +27,12 @@ public class MaxSum {
                 end = i;
             }
         }
-        for (int k = start; k <= end; k++) res.add(a[k]);
+        int res[] = new int[end - start + 1];
+        int n = 0;
+        for (int k = start; k <= end; k++) {
+            res[n] = a[k];
+            n++;
+        }
         return res;
     }
 }
--- MaxSumTest.java
+++ MaxSumTest.java
@@ -1,30 +1,34 @@
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import static org.junit.Assert.assertArrayEquals;
 
-import static org.junit.Assert.assertEquals;
+public class MaxSumTest {
 
-public class MaxSumTest{
+    int input[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int input1[] = {6, 3, 90, 88, -200, 2, 333};
+    int input2[] = {};
+    int input3[] = {-9, -12, -23, 0, -4, -9};
+    int input4[] = {10, 22, 11, 1, 0, 9, 6};
+    int input5[] = {-1, -2, -3, -4, -5, 100};
+    int input6[] = {-5, -5, 10, 10, 10, -5, -5};
 
-    int price[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    int price1[] = {6, 3, 90, 88, -200, 2, 333};
-    int price2[] = {};
-    int price3[] = {-9, -12, -23, 0, -4, -9};
-    int price4[] = {10, 22, 11, 1, 0, 9, 6};
-
-    List<Integer> res = Arrays.asList(4, -1, 2, 1);
-    List<Integer> res1 = Arrays.asList(2, 333);
-    List<Integer> res2 = Arrays.asList();
-    List<Integer> res3 = Arrays.asList();
-    List<Integer> res4 = Arrays.asList(10, 22, 11, 1, 0, 9, 6);
+    int res[] = {4, -1, 2, 1};
+    int res1[] = {2, 333};
+    int res2[] = {};
+    int res3[] = {};
+    int res4[] = {10, 22, 11, 1, 0, 9, 6};
+    int res5[] = {100};
+    int res6[] = {10, 10, 10};
 
     @Test
     public void maxSumTest() {
-        assertEquals(MaxSum.findBestDays(price), res);
-        assertEquals(MaxSum.findBestDays(price1), res1);
-        assertEquals(MaxSum.findBestDays(price2), res2);
-        assertEquals(MaxSum.findBestDays(price3), res3);
-        assertEquals(MaxSum.findBestDays(price4), res4);
+        assertArrayEquals(res, MaxSum.findMaxSum(input));
+        assertArrayEquals(res1, MaxSum.findMaxSum(input1));
+        assertArrayEquals(res2, MaxSum.findMaxSum(input2));
+        assertArrayEquals(res3, MaxSum.findMaxSum(input3));
+        assertArrayEquals(res4, MaxSum.findMaxSum(input4));
+        assertArrayEquals(res5, MaxSum.findMaxSum(input5));
+        assertArrayEquals(res6, MaxSum.findMaxSum(input6));
+
     }
 }
